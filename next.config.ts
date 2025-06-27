e208137833e44377fef582f2084595a74a1648c1
Pedro Rosa <pedro.rosa@3ainvestimentos.com.br>
2025-06-27 18:54:50 +0000

    Retornou o mesmo erro
    ola
    Pedro Rosa
    Ocorreu um erro ao se conectar com

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    GRADIO_API_KEY: process.env.GRADIO_API_KEY,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
