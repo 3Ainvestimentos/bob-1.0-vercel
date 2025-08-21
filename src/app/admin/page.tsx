e1d68fe320dcdb71f68a65fe67cb2ccc42d2cdb8
Pedro Rosa <pedro.rosa@3ainvestimentos.com.br>
2025-08-21 19:14:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.li role

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -190,7 +190,7 @@ export default function AdminPage() {
             const userDocRef = doc(db, 'users', user.uid);
             const userDocSnap = await getDoc(userDocRef);
 
-            if (userDocSnap.exists() && userDocSnap.data()?.role === 'admin') {
+            if (userDocSnap.exists && userDocSnap.data()?.role === 'admin') {
                 setIsAuthorized(true);
                 fetchAdminData();
             } else {
@@ -1367,3 +1367,5 @@ export default function AdminPage() {
     </div>
   );
 }
+
+    
